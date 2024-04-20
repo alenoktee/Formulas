28ab08708ed2438f874e7859da010403c932540c
Алёна <111691760+alenoktee@users.noreply.github.com>
2024-04-20 23:59:57 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -29,6 +29,7 @@ namespace LR23
             textBox5.Text = string.Empty;
             textBox6.Text = string.Empty;
             textBox3.Text = string.Empty;
+            textBox7.Text = string.Empty;
         }
         private void active2()
         {
@@ -50,6 +51,7 @@ namespace LR23
             textBox1.Text = string.Empty;
             textBox2.Text = string.Empty;
             textBox3.Text = string.Empty;
+            textBox7.Text = string.Empty;
         }
 
         private void button1_Click(object sender, EventArgs e)
